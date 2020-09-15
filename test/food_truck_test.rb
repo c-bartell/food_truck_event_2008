3976a2ccf325b0d8d0886a25c220d990c834a720
Curtis Bartell <cbartell980@gmail.com>
2020-09-15 13:46:30 -0600

    Add test_it_can_calculate_potential_revenue

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -51,4 +51,14 @@ class FoodTruckTest < Minitest::Test
     assert_equal expected, food_truck.inventory
     assert_equal 12, food_truck.check_stock(item2)
   end
+
+  def test_it_can_calculate_potential_revenue
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    food_truck.stock(item1, 30)
+    food_truck.stock(item2, 10)
+
+    assert_equal 136.90, food_truck.potential_revenue
+  end
 end
