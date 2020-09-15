2eb6a4cf8032e1d393e4d51464f367ad57a45df2
Curtis Bartell <cbartell980@gmail.com>
2020-09-15 14:18:38 -0600

    Add test_it_can_list_all_items

--- test/event_test.rb
+++ test/event_test.rb
@@ -6,25 +6,25 @@ require './lib/event'
 
 class EventTest < Minitest::Test
   def test_it_has_attributes
-    event = Event.new("South Pearl Street Farmers Market")
+    event = Event.new('South Pearl Street Farmers Market')
 
-    assert_equal "South Pearl Street Farmers Market", event.name
+    assert_equal 'South Pearl Street Farmers Market', event.name
     assert_equal [], event.food_trucks
   end
 
   def test_it_can_add_food_trucks
-    event = Event.new("South Pearl Street Farmers Market")
-    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
-    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    event = Event.new('South Pearl Street Farmers Market')
+    food_truck1 = FoodTruck.new('Rocky Mountain Pies')
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: '$3.75'})
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: '$5.30'})
+    item4 = Item.new({name: 'Banana Nice Cream', price: '$4.25'})
     food_truck1.stock(item1, 35)
     food_truck1.stock(item2, 7)
-    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2 = FoodTruck.new('Ba-Nom-a-Nom')
     food_truck2.stock(item4, 50)
     food_truck2.stock(item3, 25)
-    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3 = FoodTruck.new('Palisade Peach Shack')
     food_truck3.stock(item1, 65)
 
     assert_equal [], event.food_trucks
@@ -37,39 +37,39 @@ class EventTest < Minitest::Test
   end
 
   def test_it_can_list_food_truck_names
-    event = Event.new("South Pearl Street Farmers Market")
-    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
-    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    event = Event.new('South Pearl Street Farmers Market')
+    food_truck1 = FoodTruck.new('Rocky Mountain Pies')
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: '$3.75'})
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: '$5.30'})
+    item4 = Item.new({name: 'Banana Nice Cream', price: '$4.25'})
     food_truck1.stock(item1, 35)
     food_truck1.stock(item2, 7)
-    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2 = FoodTruck.new('Ba-Nom-a-Nom')
     food_truck2.stock(item4, 50)
     food_truck2.stock(item3, 25)
-    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3 = FoodTruck.new('Palisade Peach Shack')
     food_truck3.stock(item1, 65)
     event.add_food_truck(food_truck1)
     event.add_food_truck(food_truck2)
     event.add_food_truck(food_truck3)
 
-    assert_equal ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"], event.food_truck_names
+    assert_equal ['Rocky Mountain Pies', 'Ba-Nom-a-Nom', 'Palisade Peach Shack'], event.food_truck_names
   end
 
   def test_it_can_find_food_trucks_that_sell_an_item
-    event = Event.new("South Pearl Street Farmers Market")
-    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
-    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    event = Event.new('South Pearl Street Farmers Market')
+    food_truck1 = FoodTruck.new('Rocky Mountain Pies')
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: '$3.75'})
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: '$5.30'})
+    item4 = Item.new({name: 'Banana Nice Cream', price: '$4.25'})
     food_truck1.stock(item1, 35)
     food_truck1.stock(item2, 7)
-    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2 = FoodTruck.new('Ba-Nom-a-Nom')
     food_truck2.stock(item4, 50)
     food_truck2.stock(item3, 25)
-    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3 = FoodTruck.new('Palisade Peach Shack')
     food_truck3.stock(item1, 65)
     event.add_food_truck(food_truck1)
     event.add_food_truck(food_truck2)
@@ -77,5 +77,26 @@ class EventTest < Minitest::Test
 
     assert_equal [food_truck1, food_truck3], event.food_trucks_that_sell(item1)
     assert_equal [food_truck2], event.food_trucks_that_sell(item4)
+  end
+
+  def test_it_can_list_all_items
+    event = Event.new('South Pearl Street Farmers Market')
+    food_truck1 = FoodTruck.new('Rocky Mountain Pies')
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: '$3.75'})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: '$5.30'})
+    item4 = Item.new({name: 'Banana Nice Cream', price: '$4.25'})
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new('Ba-Nom-a-Nom')
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new('Palisade Peach Shack')
+    food_truck3.stock(item1, 65)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    assert_equal ['Apple Pie (Slice)', 'Banana Nice Cream', 'Peach Pie (Slice)', 'Peach-Raspberry Nice Cream'], event.sorted_item_list
   end
 end
