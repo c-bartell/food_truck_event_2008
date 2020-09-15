48a3ca079fa5797108c8239a762e28a1039b5137
Curtis Bartell <cbartell980@gmail.com>
2020-09-15 13:23:15 -0600

    Add test_it_has_attributes

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -4,4 +4,13 @@ require './lib/food_truck'
 require './lib/item'
 
 class FoodTruckTest < Minitest::Test
+  def test_it_has_attributes
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    assert_equal "Rocky Mountain Pies", food_truck.name
+
+    expected = {}
+
+    assert_equal expected, food_truck.inventory
+  end
 end
