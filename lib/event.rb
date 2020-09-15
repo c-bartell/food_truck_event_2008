894b35a59fc7a613b9f69d34024ef9d3e44080b7
Curtis Bartell <cbartell980@gmail.com>
2020-09-15 14:12:02 -0600

    Add food_truck_names

--- lib/event.rb
+++ lib/event.rb
@@ -7,6 +7,10 @@ class Event
   end
 
   def add_food_truck(food_truck)
-    @food_trucks << food_truck
+    food_trucks << food_truck
+  end
+
+  def food_truck_names
+    food_trucks.map(&:name)
   end
 end
