3b523d64d3b17ccef3cb4e2e42200e8e884e1046
Curtis Bartell <cbartell980@gmail.com>
2020-09-15 13:34:44 -0600

    Add test_it_can_stock_items

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -23,4 +23,19 @@ class FoodTruckTest < Minitest::Test
     assert_equal expected, food_truck.inventory
     assert_equal 0, food_truck.check_stock(item1)
   end
+
+  def test_it_can_stock_items
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    expected = {}
+
+    assert_equal expected, food_truck.inventory
+    assert_equal 0, food_truck.check_stock(item1)
+
+    food_truck.stock(item1, 30)
+    expected = { item1 => 30 }
+    assert_equal expected, food_truck.inventory
+    assert_equal 30, food_truck.check_stock(item1)
+  end
 end
