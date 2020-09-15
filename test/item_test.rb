1deb60ca882d078357771368a56c5c663a292cc1
Curtis Bartell <cbartell980@gmail.com>
2020-09-15 13:14:18 -0600

    Add test_it_has_attributes

--- test/item_test.rb
+++ test/item_test.rb
@@ -1,5 +1,12 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/item'
 
 class ItemTest < Minitest::Test
+  def test_it_has_attributes
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+    assert_equal "Apple Pie (Slice)", item2.name
+    assert_equal 2.50, item2.price
+  end
 end
